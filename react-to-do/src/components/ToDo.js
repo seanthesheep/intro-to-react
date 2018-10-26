cda76c20919d55ddfb6be913886c601b01c8128a
Sean Sullivan <seansullivan@Seans-MacBook-Pro.local>
2018-10-26 16:58:47 -0500

    completed events checkpoint

---
+++ react-to-do/src/components/ToDo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+  render() {
+    return (
+      <li>
+        <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
+        <span>{ this.props.description } </span>
+      <button onClick={ this.props.deleteTodo } >Delete Todo</button>
+      </li>
+    );
+  }
+}
+
+export default ToDo;
